a2544c107d6509d90dee8b73618d7dcf98a36740
tusha <tusha@DESKTOP-3NB8R7I>
2023-01-08 14:30:55 +0530

    How to Right Click in Selenium using Action Class?

---
+++ testSWD/src/testSelenium/RightClickDemo.java
@@ -0,0 +1,39 @@
+package testSelenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class RightClickDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		//Set system property for driver executable's path
+		// System Property for Chrome Driver   
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\tusha\\Downloads\\chromedriver_win32\\chromedriver.exe");  
+        
+        //Create Chrome driver's instance
+        WebDriver driver = new ChromeDriver();
+        
+        //Set implicit wait of 10 seconds
+    	//This is required for managing waits in selenium webdriver
+        
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        
+        driver.manage().window().maximize();
+        
+        //Launch sampleSiteForSelenium
+        driver.get("https://demoqa.com/buttons");
+        
+        Actions action= new Actions(driver);
+        WebElement buttonElement = driver.findElement(By.xpath("//*[@id='rightClickBtn']"));
+        action.contextClick(buttonElement).perform();
+
+	}
+
+}
